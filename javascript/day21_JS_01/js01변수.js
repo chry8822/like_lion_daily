1764666b6830f8d6d2b6d420905c3c7c68c8662b
Chrys <89508217+chry8822@users.noreply.github.com>
2021-11-29 22:33:53 +0900

    day21 JS_01 update
    day21 JS_01 update

---
+++ javascript/day21_JS_01/js01변수.js
@@ -0,0 +1,25 @@
+
+// https://ko.javascript.info/ 소개
+// https://www.w3schools.com/ 소개
+
+// 자바스크립트로 출력하는 방법
+// let age = 10; //지금은 다루지 않습니다.
+// const pi = 3.141592; // 지금은 다루지 않습니다.
+// var name = '사용자';
+// var age = 20;
+
+// document.getElementById('title').innerHTML = '오국화';
+// document.write(20 + 20);
+// window.alert(name);
+// console.log(age);
+// 한 줄 주석
+/* 주석
+입니다. */
+
+
+var myVal8 = "오국화";
+var MyVal8 = "오국화2";
+
+alert(myVal8);
+alert(MyVal8);
+
